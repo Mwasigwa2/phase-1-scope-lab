aa0b4e06b600574a00c33c657541f4dfc6afc228
Mwasigwa <135589480+Mwasigwa2@users.noreply.github.com>
2023-07-05 12:19:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+ customerName= customerName.toUpperCase ();
+ 
+}
+
+
+var customerName = 'bob'
+function setBestCustomer() {
+     bestCustomer = 'not bob';
+    
+}
+
+function overwriteBestCustomer() {
+    if (bestCustomer = 'maybe bob') {
+        return "not the best customer"
+    }
+}
+
+const leastFavoriteCustomer = 'Daniel';
+
+function changeLeastFavoriteCustomer() {
+ const leastFavoriteCustomer = 22
+    leastFavoriteCustomer = 'Peter'
+}
